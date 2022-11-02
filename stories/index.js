2ae87a5d42eb99f7030c332da5283263039e6ce1
Kevin Lee <jhssttj@hotmail.com>
2022-11-02 11:29:39 -0600

    Add confirm and show componenent to application

--- stories/index.js
+++ stories/index.js
@@ -13,6 +13,8 @@ import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment/index.js";
 import Header from "components/Appointment/Header.js";
 import Empty from "components/Appointment/Empty.js";
+import Show from "components/Appointment/Show.js";
+import Confirm from "components/Appointment/Confirm.js";
 
 storiesOf("Button", module)
   .addParameters({
@@ -143,5 +145,18 @@ storiesOf("Button", module)
   .add("Appointment", () => <Appointment />)
   .add("Appointment with Time", () => <Appointment time="12pm" />)
   .add("Header", () => <Header time = "12pm" />)
-  .add("Empty", () => <Empty onAdd ={action("onAdd")} />);
+  .add("Empty", () => <Empty onAdd ={action("onAdd")} />)
+  .add("Show", () => 
+    <Show 
+      name = "Lydia Miller-Jones"
+      interviewer = "Sylvia Palmer"
+      onEdit ={action("onEdit")} 
+      onDelete ={action("onDelete")}
+      />)
+  .add("Confirm", () => 
+    <Confirm 
+      message = "Delete the appointment?"
+      onConfirm ={action("onConfirm")} 
+      onCancel ={action("onCancel")}
+  />);
   
