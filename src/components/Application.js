938c34e29f72cb33c335c823fcf9c938abf0f848
Kevin Lee <jhssttj@hotmail.com>
2022-11-03 13:39:19 -0600

    Refactor state consts in application.js

--- src/components/Application.js
+++ src/components/Application.js
@@ -47,9 +47,17 @@ const appointments = {
 };
 
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
 
-  const [days, setDays] = useState([]);
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    // you may put the line below, but will have to remove/comment hardcoded appointments variable
+    appointments: {}
+  });
+
+  const setDay = day => setState({ ...state, day });
+  const setDays = days => setState(prev => ({ ...prev, days }));
+
 
 useEffect(() => {
   axios.get('http://localhost:8001/api/days')
@@ -69,8 +77,8 @@ useEffect(() => {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList
-            days={days}
-            value={day}
+            days={state.days}
+            value={state.day}
             onChange={setDay}/>
         </nav>
         <img
