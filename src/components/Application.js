ca7cb3fae746729656011cb5846910ca03a714f7
Kevin Lee <jhssttj@hotmail.com>
2022-11-04 12:49:49 -0600

    Add function to get interviewers for day and use it to render form

--- src/components/Application.js
+++ src/components/Application.js
@@ -6,7 +6,7 @@ import "components/Application.scss";
 import DayList from "components/DayList";
 import Appointment from "components/Appointment";
 
-import {getAppointmentsForDay, getInterview} from "../helpers/selectors";
+import {getAppointmentsForDay, getInterview, getInterviewersForDay} from "../helpers/selectors";
 
 
 export default function Application(props) {
@@ -20,6 +20,7 @@ export default function Application(props) {
 
   const setDay = day => setState({ ...state, day });
   const dailyAppointments = getAppointmentsForDay(state, state.day);
+  const interviewers = getInterviewersForDay(state, state.day)
 
 useEffect(() => {
   Promise.all([
@@ -62,6 +63,7 @@ useEffect(() => {
             id={appointment.id}
             time={appointment.time}
             interview={interview}
+            interviewers = {interviewers}
             />
           )}
         )}
